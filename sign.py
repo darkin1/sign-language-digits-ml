a9710e82d59db9d5aa5d4a5adead31fc10707a27
Dariusz Ciesielski <dciesielski87@gmail.com>
2019-02-06 15:04:36 +0100

    Init

---
+++ sign.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+import numpy as np
+
+#https://www.kaggle.com/ardamavi/sign-language-digits-dataset#Sign-language-digits-dataset.zip
+x_load = np.load('./dataset/X.npy')
+y_load = np.load('./dataset/Y.npy')
